f58381c96197af83aeae48e0619c90e760f6c2e9
jayiterator <jayson.nm.paul@hotmail.com>
2022-11-18 10:26:32 +1100

    More components

---
+++ DoneWithIt/app/components/Card/Card.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+function Card(props) {
+  return <Card style={styles.card} title="" subTitle="" image={require("")} />;
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#fff",
+  },
+});
+
+export default Card;
